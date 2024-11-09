66e89e6ed98c0306b3e4d3f5042a81b3920dbe53
Sjx0ck <tim.dimytch@gmail.com>
2024-11-08 17:39:27 -0800

    changed db

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Warehouse_4',
+        'NAME': 'Warehouse_5',
         'USER': 'postgres',
         'PASSWORD': 'Tim597707',
         'HOST': 'localhost',
